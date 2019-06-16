bdce454f04cd3b77708946933cd6460989180c55
William Mitchell <39893572+udwdmgit@users.noreply.github.com>
2019-06-16 02:46:05 -0400

    Add files via upload

---
+++ concat_1.m
@@ -0,0 +1,10 @@
+s1 = 'Good';
+s2 = 'morning';
+s = strcat(s1,s2)
+
+A  = 9.54;
+B = 5.23;
+a = num2str(A);
+b = num2str(B);
+
+s3 = strcat(a,b);
---
+++ print1.m
@@ -0,0 +1,12 @@
+m = 10.42;
+n = 9.5;
+
+%% Method 1
+fprintf('\n m = %f\n', m) 
+fprintf('\n n = %f\n\n', n)
+
+%% Method 2
+% disp(m)
+
+%% Method 3  - returns a string
+% str = sprintf('%f\n', m)
